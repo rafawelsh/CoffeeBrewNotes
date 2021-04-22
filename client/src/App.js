11337dca7147f0923500c67980beb6046a1909d3
rafawelsh <mrrafael1912@gmail.com>
2021-04-21 22:38:01 -0700

    added a 404 page & route

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar/NavBar";
+import Resources from "./components/Resources/Resources";
 import LoginForm from "./components/UserForms/LoginForm";
 import RegisterForm from "./components/UserForms/RegisterForm";
 import CoffeeForm from "./components/CoffeeForm/CoffeeForm";
 import RecipesGrid from "./components/RecipeView/RecipesGrid";
 import RecipeGridCard from "./components/RecipeView/RecipeGridCard";
+import NotFound from "./components/NotFound";
 import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
@@ -22,12 +24,14 @@ function App() {
 				<Route exact path='/' component={Home} />
 				<Route exact path='/new-recipe' component={CoffeeForm} />
 				<Route exact path='/grid' component={RecipesGrid} />
+				<Route exact path='/resources' component={Resources} />
 				<Route exact path='/login' component={LoginForm} />
 				<Route exact path='/register' component={RegisterForm} />
 				<Route
 					path={`/grid/:id`}
 					component={(props) => <RecipeGridCard {...props} />}
 				/>
+				<Route path='*' component={NotFound} />
 			</Switch>
 			<Footer />
 		</Router>
