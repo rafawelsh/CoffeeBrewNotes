ff3451cfeafb485cfb5b8e6989a1443946ea7fcc
rafawelsh <mrrafael1912@gmail.com>
2021-04-22 09:58:27 -0700

    added ProtedctedRoutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar/NavBar";
@@ -22,12 +23,23 @@ function App() {
 
 			<Switch>
 				<Route exact path='/' component={Home} />
-				<Route exact path='/new-recipe' component={CoffeeForm} />
-				<Route exact path='/grid' component={RecipesGrid} />
+				<ProtectedRoute
+					loggedIn={loggedIn}
+					exact
+					path='/new-recipe'
+					component={CoffeeForm}
+				/>
+				<ProtectedRoute
+					loggedIn={loggedIn}
+					exact
+					path='/grid'
+					component={RecipesGrid}
+				/>
 				<Route exact path='/resources' component={Resources} />
 				<Route exact path='/login' component={LoginForm} />
 				<Route exact path='/register' component={RegisterForm} />
-				<Route
+				<ProtectedRoute
+					loggedIn={loggedIn}
 					path={`/grid/:id`}
 					component={(props) => <RecipeGridCard {...props} />}
 				/>
