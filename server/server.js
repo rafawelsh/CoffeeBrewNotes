f14632a3bdc7e68fa5e21e29f9f391dedfcca60b
rafawelsh <mrrafael1912@gmail.com>
2021-05-12 21:10:46 -0700

    changing config for Heroku / Netlify connection

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ app.use(express.json());
 app.use("/api/recipes/", recipes);
 app.use("/api/user/", authRoute);
 
+app.get("/", (req, res) => {
+	res.send("Hello from Express!");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
